c77a35126a440c0bfb22cd5bedc9db14dab20a07
davier enrique perez cabrera <davierperez11.2@gmail.com>
2024-12-21 15:06:13 -0500

    feat: rename functions search binary

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
 /**
- * function applying search binary using function factorial
+ * function applying search binary using recursive function
+ * @param list
+ * @param firstIndex
+ * @param lastIndex
+ * @param numberToSearch
+ */
+function binarySearchRecursive(list: number[], firstIndex: number, lastIndex: number, numberToSearch: number): number {
+    if(lastIndex - firstIndex == 1) return -1;
+    let middleIndex: number = Math.ceil((firstIndex + lastIndex) / 2);
+    const number: number = list[middleIndex];
+    if(number == numberToSearch) return middleIndex;
+    else if(number < numberToSearch)
+        return binarySearchRecursive(list, middleIndex++, lastIndex, numberToSearch);
+    else if(number > numberToSearch)
+        return binarySearchRecursive(list, 0, middleIndex--, numberToSearch);
+    return -1;
+}
+
+/**
+ * binary search to find index of a number without use recursive function
  * @param list
  * @param firstIndex
  * @param lastIndex
  * @param numberToSearch
  */
 function binarySearch(list: number[], firstIndex: number, lastIndex: number, numberToSearch: number): number {
-    if(lastIndex - firstIndex == 1) return -1;
-    let middleIndex: number = Math.ceil((firstIndex + lastIndex) / 2);
-    const number: number = list[middleIndex];
-    if(number == numberToSearch) return middleIndex;
-    else if(number < numberToSearch)
-        return binarySearch(list, middleIndex++, lastIndex, numberToSearch);
-    else if(number > numberToSearch)
-        return binarySearch(list, 0, middleIndex--, numberToSearch);
-    return -1;
-}
-
-/**
- * binary search to find index of a number without use factorial function
- * @param list
- * @param firstIndex
- * @param lastIndex
- * @param numberToSearch
- */
-function binarySearch2(list: number[], firstIndex: number, lastIndex: number, numberToSearch: number): number {
 
     while(true) {
         if(lastIndex - firstIndex == 1) return -1;
@@ -55,6 +55,6 @@ function sortNumbers(list: number[]): number[] {
  */
 export function bs(numberList: number[], number: number): number {
     const numbersSorted: number[] = sortNumbers(numberList);
-    return binarySearch2(numbersSorted, 0, numbersSorted.length - 1, number);
+    return binarySearch(numbersSorted, 0, numbersSorted.length - 1, number);
 }
 
